e29ff2056ce064819ec2273bdcaa17b95a60b1c8
Lukasz Pawelczyk <havner@gmail.com>
2015-02-10 22:17:08 +0100

    dcs_folder: absolute paths work now

--- dcs2target_phase1.bat
+++ dcs2target_phase1.bat
@@ -1,3 +1,4 @@
+rem set DCS_FOLDER=C:\Games\DCS
 set DCS_FOLDER=..
 set LUA_CPATH=%DCS_FOLDER%\bin\lua-?.dll;%DCS_FOLDER%\bin\?.dll
 %DCS_FOLDER%\bin\luae.exe dcs2target_parser.lua %DCS_FOLDER% %1 %2 %3 %4 %5 %6 %7 %8 %9
